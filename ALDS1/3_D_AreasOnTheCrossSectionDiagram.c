95f01dae0723094da1d91bf16dc2befdd9ef5c43
tsoushi <79128170+tsoushi@users.noreply.github.com>
2022-12-16 11:26:12 +0900

    変数名が不適切だったので変更

--- ALDS1/3_D_AreasOnTheCrossSectionDiagram.c
+++ ALDS1/3_D_AreasOnTheCrossSectionDiagram.c
@@ -9,9 +9,9 @@ typedef struct {
 
 int main() {
     int leftSideStack[20000]; // '\'が出現したインデックスを保持するスタック
-    int leftSideStackTail = 0;
+    int leftSideStackTop = 0;
     Puddle puddles[10000]; // 水たまりのスタック
-    int puddlesTail = 0;
+    int puddlesTop = 0;
     char inStr[20000];
     int areaSum = 0;
 
@@ -19,28 +19,28 @@ int main() {
 
     for (int i = 0;; i++) {
         if (inStr[i] == '\\') {
-            leftSideStack[leftSideStackTail++] = i;
+            leftSideStack[leftSideStackTop++] = i;
         }
-        else if (inStr[i] == '/' && leftSideStackTail > 0) {
-            int leftSideIndex = leftSideStack[--leftSideStackTail]; // 直近の'\'のインデックスを取得する
+        else if (inStr[i] == '/' && leftSideStackTop > 0) {
+            int leftSideIndex = leftSideStack[--leftSideStackTop]; // 直近の'\'のインデックスを取得する
             int area = i - leftSideIndex;
             areaSum += area;
 
             // 水たまりの統合を行う
             //     現在処理中の水たまりの左端より右側にある水たまりは統合を行う
-            while (puddlesTail && puddles[puddlesTail - 1].leftSideIndex > leftSideIndex) {
-                area += puddles[--puddlesTail].area;
+            while (puddlesTop && puddles[puddlesTop - 1].leftSideIndex > leftSideIndex) {
+                area += puddles[--puddlesTop].area;
             }
-            puddles[puddlesTail].leftSideIndex = leftSideIndex;
-            puddles[puddlesTail++].area = area;
+            puddles[puddlesTop].leftSideIndex = leftSideIndex;
+            puddles[puddlesTop++].area = area;
         }
         else if (inStr[i] == '\0') break;
     }
 
     // 結果を出力
     printf("%d\n", areaSum);
-    printf("%d", puddlesTail);
-    for (int i = 0; i < puddlesTail; i++) {
+    printf("%d", puddlesTop);
+    for (int i = 0; i < puddlesTop; i++) {
         printf(" %d", puddles[i].area);
     }
     printf("\n");
